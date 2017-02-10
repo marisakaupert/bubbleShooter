b9636b1fbe45c5c30a5757b325a78648c4828244
Marisa Lozano <marisalozano1995@gmail.com>
2017-02-09 22:57:29 -0500

    fix root

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ var paths = {
 
 gulp.task('serveprod', function() {
   connect.server({
-    root: './',
+    root: [__dirname],
     port: process.env.PORT || 5000, // localhost:5000
     livereload: true
   });
