c169ca4e77b72135a1d47bf98fa2ca925efccdc0
Marisa Lozano <marisalozano1995@gmail.com>
2017-02-16 18:39:33 -0500

    adding more js stuff

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,13 @@
 var BubbleShoot = window.BubbleShoot || {};
 BubbleShoot.Game = (function($){
     var Game = function(){};
+    rhis.init = function() {
+        $(".playButton").bind('click',startGame);
+    };
+    var startGame = function() {
+        $(".playButton").unbind('click');
+        BubbleShoot.ui.hideDialog();
+    };
+    };
     return Game;
 })(jQuery);
