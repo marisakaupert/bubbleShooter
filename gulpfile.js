d7dab92acbe24234ed2cf933d6bf97a08c75f87e
Marisa Lozano <marisalozano1995@gmail.com>
2017-03-30 16:07:21 -0400

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ var paths = {
 gulp.task('serveprod', function() {
   connect.server({
     root: [__dirname],
-    port: process.env.PORT || 5000, // localhost:5000
+    port: process.env.PORT || 8000, // localhost:5000
     livereload: false
   });
 });
